81fee3f872358fba469a10b75cd13314c26d51e1
Hunter Lannon <kindlehl@osuosl.org>
2019-04-14 13:14:13 -0700

    readded tests

--- bag.hs
+++ bag.hs
@@ -1,5 +1,5 @@
 module HW1types where
- 
+
 import Data.List (nub,sort)
 import qualified Data.Set as Set
 
@@ -7,31 +7,35 @@ type Node  = Int
 type Edge  = (Node,Node)
 type Graph = [Edge]
 type Path  = [Node]
+---------------------------------------------- Bags
+
 type Bag a = [(a,Int)]
- 
+
 norm :: Ord a => [a] -> [a]
 norm = sort . nub
- 
+
 nullList  = []
 list = [2,3,3,5,7,7,7,8]
 bag1 :: Bag Int
 bag2 :: Bag Int
-bag1 = [(5,1),(7,3)]
-bag2 = [(5,1),(3,6)]
- 
+
 {- | Insert item into bag
 >>> ins 5 bag1
 [(5,2),(7,3)]
 
 >>> ins 5 []
 [(5,1)]
-
 -}
+
+bag1 = [(5,1),(7,3),(2,1),(3,2),(8,1)]
+bag2 = [(5,1),(3,6),(1,0),(8,7),(4,1)]
+
+---------------------------------------------- 1-a
 ins :: Eq a  => a -> Bag a -> Bag a
 ins x []     = [(x,1)]
-ins x (y:ys) = if x == fst y
-               then (x,succ(snd y)):ys
-               else y : (ins x ys)
+ins x (y:ys) | x == fst y = (x,succ(snd y)):ys
+			 | otherwise  = y : (ins x ys)
+
 {- | Delete item from bag
 >>> del 5 bag1
 [(7,3)]
@@ -58,8 +62,32 @@ build_bag :: Eq a => [a] -> Bag a -> Bag a
 build_bag [] baggy = baggy
 build_bag (x:xs) baggy = ins x (build_bag xs baggy)
 
+
+---------------------------------------------- 1-c
 bag :: Eq a => [a] -> Bag a
+bag []     = []
 bag xs = build_bag xs []
+
+---------------------------------------------- 1-d
+subbag :: Eq a => Bag a -> Bag a -> Bool
+subbag xs [] = False
+subbag [] ys = True
+subbag (x:xs) ys = check x ys && subbag xs ys
+
+check x []	   = False
+check x (y:ys) | x == y = True
+			   | otherwise = check x ys
+
+---------------------------------------------- 1-e
+isbag :: Eq a 	=> Bag a -> Bag a -> Bag a
+isbag [] ys     = []
+isbag (x:xs) ys | check x ys = x : isbag xs ys
+				| otherwise  = isbag xs ys
+
+---------------------------------------------- 1-f
+size :: Bag a -> Int
+size [] = 0
+size (x:xs) = snd x + size xs
 
 
 ---------------------------------------------- 2-a
